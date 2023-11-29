df1dcfeb87b2e0af1a6c4cb11929c78b2e56bcaa
Lalitkumar4 <64465383+Lalitkumar4@users.noreply.github.com>
2023-11-29 14:39:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  AreaChart,
+  Area,
+  BarChart,
+  Bar,
+} from "recharts";
+
+const pdata = [
+  {
+    name: "Python",
+    student: 13,
+    fees: 10,
+  },
+  {
+    name: "JavaScript",
+    student: 15,
+    fees: 12,
+  },
+  {
+    name: "PHP",
+    student: 5,
+    fees: 10,
+  },
+  {
+    name: "Java",
+    student: 10,
+    fees: 5,
+  },
+  {
+    name: "C#",
+    student: 10,
+    fees: 8,
+  },
+  {
+    name: "C++",
+    student: 10,
+    fees: 8,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h1 className="chart-heading">Line Chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          data={pdata}
+          width={500}
+          height={300}
+          margin={{ top: 5, right: 100, left: 20, bottom: 5 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="name"
+            interval={"preserveStartEnd"}
+            tickFormatter={(value) => value + "Programming"}
+          />
+          <YAxis />
+          <Tooltip contentStyle={{ backgroundColor: "yellow" }} />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="student"
+            stroke="red"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="fees"
+            stroke="green"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <h1 className="chart-heading">Area Chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <AreaChart
+          data={pdata}
+          width={500}
+          height={300}
+          margin={{ top: 5, right: 100, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area
+            type="monotone"
+            dataKey="student"
+            stroke="#88884d8"
+            fill="#8884d8"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+
+      <h1 className="chart-heading">Bar Chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <BarChart
+          data={pdata}
+          width={500}
+          height={300}
+          margin={{ top: 5, right: 100, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="student" fill="#8884d8" />
+          <Bar dataKey="fees" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
